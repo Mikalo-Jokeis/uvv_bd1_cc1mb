6cb4a49c94e8cf1d44615608b8e425c08cb07cb0
Mikalo-Jokeis <124303348+Mikalo-Jokeis@users.noreply.github.com>
2023-05-28 14:33:42 -0300

    Add files via upload

--- pset1/cc1mb_202305768_postgresql.sql
+++ pset1/cc1mb_202305768_postgresql.sql
@@ -15,15 +15,19 @@ ENCRYPTED PASSWORD 'NeverGonnaGiveYouUp!';
 
 
 -- Criar o banco de dados uvv com os parâmetros
-CREATE 	DATABASE uvv 
-WITH 	OWNER michel
-		TEMPLATE template0
-		ENCODING 'UTF8'
-		LC_COLLATE 'pt_BR.UTF-8'
-		LC_CTYPE 'pt_BR.UTF-8'
-		ALLOW_CONNECTIONS TRUE;
+CREATE 	DATABASE 			uvv 
+WITH 	OWNER 				michel
+		TEMPLATE 			template0
+		ENCODING 			'UTF8'
+		LC_COLLATE 			'pt_BR.UTF-8'
+		LC_CTYPE 			'pt_BR.UTF-8'
+		ALLOW_CONNECTIONS 	TRUE;
 
 	
+-- Criar comentário referente ao banco de dados uvv
+COMMENT ON DATABASE uvv 	IS 		'Banco de dados da Lojas UVV';
+
+
 -- Dar todos os privilégios para o usuário michel
 GRANT ALL PRIVILEGES ON DATABASE uvv TO michel;
 
@@ -55,13 +59,10 @@ CREATE TABLE 	lojas.produtos (
                 imagem_ultima_atualizacao 	DATE);
 
 
--- Criar as chaves da tabela produtos
-ALTER TABLE 	lojas.produtos
-ADD CONSTRAINT 	produtos_pk
-PRIMARY KEY 	(produto_id);
-
-
--- Criar comentário referente a tabela produtos e criar comentários referente as colunas da tabela produtos
+/* 
+ * Criar comentário referente a tabela produtos
+ * Criar comentários referentes as colunas da tabela produtos
+ */
 COMMENT ON TABLE  lojas.produtos 							IS 	'Tabela com as informações dos produtos.';
 COMMENT ON COLUMN lojas.produtos.produto_id 				IS 	'Chave Primária (PK) da tabela produtos. Número do id de identificação do produto.';
 COMMENT ON COLUMN lojas.produtos.nome 						IS 	'Nome do produto.';
@@ -74,6 +75,16 @@ COMMENT ON COLUMN lojas.produtos.imagem_charset 			IS 	'Tipo de codificação do
 COMMENT ON COLUMN lojas.produtos.imagem_ultima_atualizacao 	IS  'Data da última atualização do arquivo da imagem do produto.';
 
 
+-- Criar as chaves da tabela produtos
+ALTER TABLE 	lojas.produtos
+ADD CONSTRAINT 	produtos_pk
+PRIMARY KEY 	(produto_id);
+
+
+-- Criar comentário referente a chave primária da tabela produtos
+COMMENT ON CONSTRAINT produtos_pk 	ON lojas.produtos	IS 	'Defini a coluna produto_id da tabela produtos como a chave primária (PK) da tabela produtos';
+
+
 -- Criar as restrições de checagem para a tabela produtos
 ALTER TABLE 	lojas.produtos
 ADD CONSTRAINT 	check_produtos_produto_id
@@ -81,11 +92,17 @@ CHECK 			(produto_id > 0);
 
 ALTER TABLE 	lojas.produtos
 ADD CONSTRAINT 	check_produtos_preco_unitario
-CHECK 			(preco_unitario >= 0);
+CHECK 			(preco_unitario > 0);
 
 ALTER TABLE 	lojas.produtos
 ADD CONSTRAINT 	check_produtos_imagem_charset
 CHECK 			(imagem_charset IN ('UTF-8','UTF-16','UTF-32'));
+
+
+-- Criar comentários referentes as restrições da tabela produtos
+COMMENT ON CONSTRAINT check_produtos_produto_id 		ON lojas.produtos	IS 	'Verifica se o produto_id é um número maior que 0';
+COMMENT ON CONSTRAINT check_produtos_preco_unitario 	ON lojas.produtos	IS 	'Verifica se o preco_unitario é um número maior que 0';
+COMMENT ON CONSTRAINT check_produtos_imagem_charset 	ON lojas.produtos	IS 	'Defini apenas 3 valores válidos para a coluna imagem_charset';
 
 
 -- Criar a tabela lojas
@@ -103,13 +120,10 @@ CREATE TABLE 	lojas.lojas (
                 logo_ultima_atualizacao 	DATE);
 
 
--- Criar as chaves da tabela lojas
-ALTER TABLE 	lojas.lojas
-ADD CONSTRAINT 	lojas_pk
-PRIMARY KEY 	(loja_id);
-
-
--- Criar comentário referente a tabela lojas e criar comentários referente as colunas da tabela lojas
+/* 
+ * Criar comentário referente a tabela lojas
+ * Criar comentários referentes as colunas da tabela lojas
+ */
 COMMENT ON TABLE  lojas.lojas 							IS 	'Tabela com as informações das lojas.';
 COMMENT ON COLUMN lojas.lojas.loja_id 					IS 	'Chave Primária (PK) da tabela lojas. Número do id de identificação da loja.';
 COMMENT ON COLUMN lojas.lojas.nome					 	IS 	'Nome da loja.';
@@ -122,6 +136,15 @@ COMMENT ON COLUMN lojas.lojas.logo_mime_type 			IS 	'MIME-type da imagem da loja
 COMMENT ON COLUMN lojas.lojas.logo_arquivo 				IS 	'Tipo de arquivo da logo da loja.';
 COMMENT ON COLUMN lojas.lojas.logo_charset 				IS 	'Tipo de codificação dos caracteres do arquivo da logo da loja.';
 COMMENT ON COLUMN lojas.lojas.logo_ultima_atualizacao 	IS 	'Data da última atualização do arquivo da logo da loja.';
+
+
+-- Criar as chaves da tabela lojas
+ALTER TABLE 	lojas.lojas
+ADD CONSTRAINT 	lojas_pk
+PRIMARY KEY 	(loja_id);
+
+-- Criar comentário referente a chave primária da tabela lojas
+COMMENT ON CONSTRAINT lojas_pk 	ON lojas.lojas	IS 	'Defini a coluna loja_id da tabela lojas como a chave primária (PK) da tabela lojas';
 
 
 -- Criar as restrições de checagem para a tabela lojas
@@ -138,6 +161,12 @@ ADD CONSTRAINT 	check_lojas_logo_charset
 CHECK 			(logo_charset IN ('UTF-8','UTF-16','UTF-32'));
 
 
+-- Criar comentários referentes as restrições da tabela lojas
+COMMENT ON CONSTRAINT check_lojas_endereco_fisico_or_web 	ON lojas.lojas	IS 	'Defini que precisa, pelo menos, ou a coluna endereco_web ou a coluna endereco_fisico estar preenchida';
+COMMENT ON CONSTRAINT check_lojas_loja_id					ON lojas.lojas	IS 	'Verifica se a loja_id é um número maior que 0';
+COMMENT ON CONSTRAINT check_lojas_logo_charset		 		ON lojas.lojas	IS 	'Defini apenas 3 valores válidos para a coluna logo_charset';
+
+
 -- Criar a tabela estoques
 CREATE TABLE 	lojas.estoques (
                 estoque_id 		NUMERIC(38) 	NOT NULL,
@@ -146,13 +175,10 @@ CREATE TABLE 	lojas.estoques (
                 quantidade 		NUMERIC(38) 	NOT NULL);
 
 
--- Criar as chaves da tabela estoques
-ALTER TABLE 	lojas.estoques
-ADD CONSTRAINT 	estoques_pk
-PRIMARY KEY 	(estoque_id);
-
-
--- Criar comentário referente a tabela estoques e criar comentários referente as colunas da tabela estoques
+/*
+ * Criar comentário referente a tabela estoques
+ * Criar comentários referentes as colunas da tabela estoques
+ */
 COMMENT ON TABLE  lojas.estoques 				IS 	'Tabela com as informações dos estoques.';
 COMMENT ON COLUMN lojas.estoques.estoque_id 	IS 	'Chave Primária (PK) da tabela estoques. Número do id de identificação do estoque.';
 COMMENT ON COLUMN lojas.estoques.loja_id 		IS 	'Chave Estrangeira (FK) da Chave Primária (PK) loja_id da tabela lojas. Número do id de identificação da loja.';
@@ -160,7 +186,17 @@ COMMENT ON COLUMN lojas.estoques.produto_id 	IS 	'Chave Estrangeira (FK) da Chav
 COMMENT ON COLUMN lojas.estoques.quantidade 	IS 	'Quantidade de determinado produto.';
 
 
--- Criar as relações referente a tabela estoques
+-- Criar as chaves da tabela estoques
+ALTER TABLE 	lojas.estoques
+ADD CONSTRAINT 	estoques_pk
+PRIMARY KEY 	(estoque_id);
+
+
+-- Criar comentário referente a chave primária da tabela estoques
+COMMENT ON CONSTRAINT estoques_pk 	ON lojas.estoques	IS 	'Defini a coluna estoque_id da tabela estoques como a chave primária (PK) da tabela estoques';
+
+
+-- Criar as relações referentes a tabela estoques
 ALTER TABLE 	lojas.estoques 
 ADD CONSTRAINT 	produtos_estoques_fk
 FOREIGN KEY 	(produto_id)
@@ -178,6 +214,11 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 
+-- Criar comentários referentes as relações da tabela estoques
+COMMENT ON CONSTRAINT produtos_estoques_fk		ON lojas.estoques	IS 	'Defini a coluna produto_id da tabela estoques como uma chave estrangeira (FK) da coluna produto_id da tabela produtos';
+COMMENT ON CONSTRAINT lojas_estoques_fk			ON lojas.estoques	IS 	'Defini a coluna loja_id da tabela estoques como uma chave estrangeira (FK) da coluna loja_id da tabela lojas';
+
+
 -- Criar as restrições de checagem para a tabela estoques
 ALTER TABLE 	lojas.estoques
 ADD CONSTRAINT 	check_estoques_estoque_id
@@ -186,6 +227,11 @@ CHECK 			(estoque_id > 0);
 ALTER TABLE 	lojas.estoques
 ADD CONSTRAINT 	check_estoques_quantidade
 CHECK 			(quantidade >= 0);
+
+
+-- Criar comentários referentes as restrições da tabela estoques
+COMMENT ON CONSTRAINT check_estoques_estoque_id		ON lojas.estoques	IS 	'Verifica se o estoque_id é um número maior que 0';
+COMMENT ON CONSTRAINT check_estoques_quantidade		ON lojas.estoques	IS 	'Verifica se a quantidade é um número maior, ou igual, que 0';
 
 
 -- Criar a tabela clientes
@@ -198,13 +244,10 @@ CREATE TABLE 	lojas.clientes (
                 telefone3 	VARCHAR(20));
 
 
--- Criar as chaves da tabela clientes
-ALTER TABLE 	lojas.clientes
-ADD CONSTRAINT 	clientes_pk
-PRIMARY KEY 	(cliente_id);
-
-
--- Criar comentário referente a tabela clientes e criar comentários referente as colunas da tabela clientes
+/*
+ * Criar comentário referente a tabela clientes
+ * Criar comentários referentes as colunas da tabela clientes
+ */
 COMMENT ON TABLE  lojas.clientes 				IS 	'Tabela com as informações dos clientes.';
 COMMENT ON COLUMN lojas.clientes.cliente_id 	IS 	'Chave Primária (PK) da tabela clientes. Número do id de identificação do cliente.';
 COMMENT ON COLUMN lojas.clientes.email 			IS 	'endereço de email do cliente.';
@@ -212,6 +255,16 @@ COMMENT ON COLUMN lojas.clientes.nome 			IS 	'Nome completo do cliente.';
 COMMENT ON COLUMN lojas.clientes.telefone1 		IS 	'1º telefone para contato do cliente.';
 COMMENT ON COLUMN lojas.clientes.telefone2 		IS 	'2º telefone para contato do cliente.';
 COMMENT ON COLUMN lojas.clientes.telefone3 		IS 	'3º telefone para contato do cliente.';
+
+
+-- Criar as chaves da tabela clientes
+ALTER TABLE 	lojas.clientes
+ADD CONSTRAINT 	clientes_pk
+PRIMARY KEY 	(cliente_id);
+
+
+-- Criar comentário referente a chave primária da tabela clientes
+COMMENT ON CONSTRAINT clientes_pk 	ON lojas.clientes	IS 	'Defini a coluna cliente_id da tabela clientes como a chave primária (PK) da tabela clientes';
 
 
 -- Criar as restrições de checagem para a tabela clientes
@@ -228,6 +281,12 @@ ADD CONSTRAINT 	check_clientes_nome
 CHECK 			(nome ~ '^\w+\s\w+');
 
 
+-- Criar comentários referentes as restrições da tabela clientes
+COMMENT ON CONSTRAINT check_clientes_cliente_id	ON lojas.clientes	IS 	'Verifica se o cliente_id é um número maior que 0';
+COMMENT ON CONSTRAINT check_clientes_email		ON lojas.clientes	IS 	'Verifica se o email tem "@" e termina com "com"';
+COMMENT ON CONSTRAINT check_clientes_nome		ON lojas.clientes	IS 	'Verifica se o nome tem pelo um conjunto de caracteres no início, um espaço, e um outro conjunto de caracteres';
+
+
 -- Criar a tabela envios
 CREATE TABLE 	lojas.envios (
                 envio_id 			NUMERIC(38) 	NOT NULL,
@@ -237,13 +296,10 @@ CREATE TABLE 	lojas.envios (
                 status 				VARCHAR(15) 	NOT NULL);
 
 
--- Criar as chaves da tabela envios
-ALTER TABLE 	lojas.envios
-ADD CONSTRAINT 	envios_pk
-PRIMARY KEY 	(envio_id);
-
-
--- Criar comentário referente a tabela envios e criar comentários referente as colunas da tabela envios
+/*
+ * Criar comentário referente a tabela envios
+ * Criar comentários referentes as colunas da tabela envios
+ */
 COMMENT ON TABLE  lojas.envios 						IS 	'Tabela com as informações dos envios.';
 COMMENT ON COLUMN lojas.envios.envio_id 			IS 	'Chave Primária (PK) da tabela envios. Número do id de identificação do envio.';
 COMMENT ON COLUMN lojas.envios.loja_id 				IS 	'Chave Estrangeira (FK) da Chave Primária (PK) loja_id da tabela lojas. Número do id de identificação da loja.';
@@ -252,7 +308,17 @@ COMMENT ON COLUMN lojas.envios.endereco_entrega 	IS 	'Endereço de entrega do en
 COMMENT ON COLUMN lojas.envios.status 				IS 	'Status da entrega do envio.';
 
 
--- Criar as relações referente a tabela envios
+-- Criar as chaves da tabela envios
+ALTER TABLE 	lojas.envios
+ADD CONSTRAINT 	envios_pk
+PRIMARY KEY 	(envio_id);
+
+
+-- Criar comentário referente a chave primária da tabela envios
+COMMENT ON CONSTRAINT envios_pk 	ON lojas.envios		IS 'Defini a coluna envio_id da tabela envios como a chave primária (PK) da tabela envios';
+
+
+-- Criar as relações referentes a tabela envios
 ALTER TABLE 	lojas.envios 
 ADD CONSTRAINT 	lojas_envios_fk
 FOREIGN KEY 	(loja_id)
@@ -270,6 +336,11 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 
+-- Criar comentários referentes as relações da tabela envios
+COMMENT ON CONSTRAINT lojas_envios_fk		ON lojas.envios	IS 	'Defini a coluna loja_id da tabela envios como uma chave estrangeira (FK) da coluna loja_id da tabela lojas';
+COMMENT ON CONSTRAINT clientes_envios_fk	ON lojas.envios	IS 	'Defini a coluna cliente_id da tabela envios como uma chave estrangeira (FK) da coluna cliente_id da tabela clientes';
+
+
 -- Criar as restrições de checagem para a tabela envios
 ALTER TABLE 	lojas.envios
 ADD CONSTRAINT 	check_envios_status
@@ -278,6 +349,11 @@ CHECK 			(status IN ('CRIADO','ENVIADO','TRANSITO','ENTREGUE'));
 ALTER TABLE 	lojas.envios
 ADD CONSTRAINT 	check_envios_envio_id
 CHECK 			(envio_id > 0);
+
+
+-- Criar comentários referentes as restrições da tabela envios
+COMMENT ON CONSTRAINT check_envios_status		ON lojas.envios		IS 	'Defini apenas 4 valores válidos para a coluna status';
+COMMENT ON CONSTRAINT check_envios_envio_id		ON lojas.envios		IS 	'Verifica se o envio_id é um número maior que 0';
 
 
 -- Criar a tabela pedidos
@@ -289,13 +365,10 @@ CREATE TABLE 	lojas.pedidos (
                 loja_id 		NUMERIC(38) 	NOT NULL);
 
 
--- Criar as chaves da tabela pedidos
-ALTER TABLE 	lojas.pedidos
-ADD CONSTRAINT 	pedidos_pk
-PRIMARY KEY 	(pedido_id);
-
-
--- Criar comentário referente a tabela pedidos e criar comentários referente as colunas da tabela pedidos
+/*
+ * Criar comentário referente a tabela pedidos
+ * Criar comentários referentes as colunas da tabela pedidos
+ */
 COMMENT ON TABLE  lojas.pedidos 				IS 	'Tabela com as informações dos pedidos.';
 COMMENT ON COLUMN lojas.pedidos.pedido_id 		IS 	'Chave Primária (PK) da tabela pedidos. Número do id de identificação do pedido.';
 COMMENT ON COLUMN lojas.pedidos.data_hora 		IS 	'data e hora de registro do pedido.';
@@ -304,7 +377,17 @@ COMMENT ON COLUMN lojas.pedidos.status 			IS 	'Status do pedido do cliente.';
 COMMENT ON COLUMN lojas.pedidos.loja_id 		IS 	'Chave Estrangeira (FK) da Chave Primária (PK) loja_id da tabela lojas. Número do id de identificação da loja.';
 
 
--- Criar as relações referente a tabela pedidos
+-- Criar as chaves da tabela pedidos
+ALTER TABLE 	lojas.pedidos
+ADD CONSTRAINT 	pedidos_pk
+PRIMARY KEY 	(pedido_id);
+
+
+-- Criar comentário referente a chave primária da tabela pedidos
+COMMENT ON CONSTRAINT pedidos_pk 	ON lojas.pedidos	IS 	'Defini a coluna pedido_id da tabela pedidos como a chave primária (PK) da tabela pedidos';
+
+
+-- Criar as relações referentes a tabela pedidos
 ALTER TABLE 	lojas.pedidos 
 ADD CONSTRAINT 	lojas_pedidos_fk
 FOREIGN KEY 	(loja_id)
@@ -322,6 +405,11 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 
+-- Criar comentários referentes as relações da tabela pedidos
+COMMENT ON CONSTRAINT lojas_pedidos_fk		ON lojas.pedidos	IS 	'Defini a coluna loja_id da tabela pedidos como uma chave estrangeira (FK) da coluna loja_id da tabela lojas';
+COMMENT ON CONSTRAINT clientes_pedidos_fk	ON lojas.pedidos	IS 	'Defini a coluna cliente_id da tabela pedidos como uma chave estrangeira (FK) da coluna cliente_id da tabela clientes';
+
+
 -- Criar as restrições de checagem para a tabela pedidos
 ALTER TABLE 	lojas.pedidos
 ADD CONSTRAINT 	check_pedidos_status
@@ -330,6 +418,11 @@ CHECK 			(status IN ('CANCELADO','COMPLETO','ABERTO','PAGO','REEMBOLSADO','ENVIA
 ALTER TABLE 	lojas.pedidos
 ADD CONSTRAINT 	check_pedidos_pedido_id
 CHECK 			(pedido_id > 0);
+
+
+-- Criar comentários referentes as restrições da tabela pedidos
+COMMENT ON CONSTRAINT check_pedidos_status		ON lojas.pedidos	IS 	'Defini apenas 6 valores válidos para a coluna status';
+COMMENT ON CONSTRAINT check_pedidos_pedido_id	ON lojas.pedidos	IS 	'Verifica se o pedido_id é um número maior que 0';
 
 
 -- Criar a tabela pedidos_itens
@@ -342,13 +435,10 @@ CREATE TABLE 	lojas.pedidos_itens (
                 envio_id 			NUMERIC(38));
 
 
--- Criar as chaves da tabela pedidos_itens
-ALTER TABLE 	lojas.pedidos_itens
-ADD CONSTRAINT 	pedidos_itens_pk
-PRIMARY KEY 	(pedido_id, produto_id);
-
-
--- Criar comentário referente a tabela pedidos_itens e criar comentários referente as colunas da tabela pedidos_itens
+/*
+ * Criar comentário referente a tabela pedidos_itens
+ * Criar comentários referentes as colunas da tabela pedidos_itens
+ */
 COMMENT ON TABLE  lojas.pedidos_itens 					IS 	'Tabela com as informações sobre os itens dos pedidos.';
 COMMENT ON COLUMN lojas.pedidos_itens.pedido_id 		IS 	'Número do id de identificação do pedido.';
 COMMENT ON COLUMN lojas.pedidos_itens.produto_id 		IS 	'Número do id de identificação do produto.';
@@ -358,7 +448,17 @@ COMMENT ON COLUMN lojas.pedidos_itens.quantidade 		IS 	'Quantidade de um determi
 COMMENT ON COLUMN lojas.pedidos_itens.envio_id 			IS 	'Chave Estrangeira (FK) da Chave Primária (PK) envio_id da tabela envios. Número do id de identificação do envio.';
 
 
--- Criar as relações referente a tabela pedidos_itens
+-- Criar as chaves da tabela pedidos_itens
+ALTER TABLE 	lojas.pedidos_itens
+ADD CONSTRAINT 	pedidos_itens_pk
+PRIMARY KEY 	(pedido_id, produto_id);
+
+
+-- Criar comentário referente a chave primária composta da tabela pedidos_itens
+COMMENT ON CONSTRAINT pedidos_itens_pk 	ON lojas.pedidos_itens	IS 	'Defini as colunas pedido_id e produto_id da tabela pedidos_itens como a chave primária (PK) composta da tabela pedidos_itens';
+
+
+-- Criar as relações referentes a tabela pedidos_itens
 ALTER TABLE 	lojas.pedidos_itens 
 ADD CONSTRAINT 	produtos_pedidos_itens_fk
 FOREIGN KEY 	(produto_id)
@@ -384,6 +484,12 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 
+-- Criar comentários referentes as relações da tabela pedidos_itens
+COMMENT ON CONSTRAINT produtos_pedidos_itens_fk 	ON lojas.pedidos_itens	IS 	'Defini a coluna produto_id da tabela pedidos_itens como uma chave estrangeira (FK) da coluna produto_id da tabela produtos';
+COMMENT ON CONSTRAINT envios_pedidos_itens_fk		ON lojas.pedidos_itens	IS 	'Defini a coluna envio_id da tabela pedidos_itens como uma chave estrangeira (FK) da coluna envio_id da tabela envios';
+COMMENT ON CONSTRAINT pedidos_pedidos_itens_fk		ON lojas.pedidos_itens	IS 	'Defini a coluna pedido_id da tabela pedidos_itens como uma chave estrangeira (FK) da coluna pedido_id da tabela pedidos';
+
+
 -- Criar as restrições de checagem para a tabela pedidos_itens
 ALTER TABLE 	lojas.pedidos_itens
 ADD CONSTRAINT 	check_pedidos_itens_numero_da_linha
@@ -396,4 +502,11 @@ CHECK 			(preco_unitario > 0);
 ALTER TABLE 	lojas.pedidos_itens
 ADD CONSTRAINT 	check_pedidos_itens_quantidade
 CHECK 			(quantidade >= 0);
+
+
+-- Criar comentários referentes as restrições da tabela pedidos_itens
+COMMENT ON CONSTRAINT check_pedidos_itens_numero_da_linha 	ON lojas.pedidos_itens	IS 	'Verifica se o numero_da_linha é um número maior que 0';
+COMMENT ON CONSTRAINT check_pedidos_itens_preco_unitario	ON lojas.pedidos_itens	IS 	'Verifica se o preco_unitario é um número maior que 0';
+COMMENT ON CONSTRAINT check_pedidos_itens_quantidade		ON lojas.pedidos_itens	IS 	'Verifica se a quantidade é um número maior, ou igual, que 0';
+
 
